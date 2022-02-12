ef940125348f1749fae93715a3fad3e0a1f446e6
Patrick Ingle <phkcorp2005@gmail.com>
2022-02-12 12:34:28 -0500

    fixes in the implementation to use modifiers

--- Security_token_offering.sol
+++ Security_token_offering.sol
@@ -6,7 +6,8 @@ contract security_token {
 
     string public constant name = "security_token";
     string public constant symbol = "STO";
-    uint8 public constant decimals = 18;  
+    // POI: 2022-02-12: security tokens are whole numbers because they represent shares in a company
+    uint8 public constant decimals = 0;
 
 
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
@@ -15,54 +16,94 @@ contract security_token {
 
     mapping(address => uint256) balance;
     mapping(address => mapping (address => uint256)) allowed;
-    
+
     uint256 totalSupply_;
 
-    // set total supply 
-   constructor(uint256 total) public {  
-	totalSupply_ = total;
-	balance[msg.sender] = totalSupply_;
-    }  
+    address owner;
+
+    // set total supply
+   constructor(uint256 total) public {
+       owner = msg.sender;
+	   totalSupply_ = total;
+	   balance[msg.sender] = totalSupply_;
+    }
+
+    modifier onlyOwner() {
+        require (msg.sender == owner, "unauthorized! only by owner");
+        _;
+    }
+
+    modifier ownerSufficientBalance(uint256 amount) {
+        require (balance[owner] >= amount, "insufficient balance remaining in owner's account");
+        _;
+    }
+
+    modifier delegateSufficientBalance(uint256 amount) {
+        require (allowed[owner][msg.sender] >= amount, "insufficient balance remaining in delegate's account");
+        _;
+    }
 
     // retrieve total supply
-    function totalSupply() public view returns (uint256) {
-	return totalSupply_;
+    function totalSupply()
+        public
+        view
+        returns
+        (uint256)
+    {
+	    return totalSupply_;
     }
-    
+
     // retrieve owner's balance
-    function balanceOf(address tokenOwner) public view returns (uint) {
+    function balanceOf(address tokenOwner)
+        public
+        view
+        returns (uint)
+    {
         return balance[tokenOwner];
     }
 
-    function transfer(address receiver, uint numTokens) public returns (bool) {
-        require(numTokens <= balance[msg.sender]); // owner must have enough tokens for transfer
-        
-        balance[msg.sender] = balance[msg.sender]-numTokens; // only owner can approve transfers at STO stage 
+    function transfer(address receiver, uint numTokens)
+        public
+        ownerSufficientBalance(numTokens)
+        returns (bool)
+    {
+        balance[msg.sender] = balance[msg.sender]-numTokens; // only owner can approve transfers at STO stage
         balance[receiver] = balance[receiver] + numTokens;
         emit Transfer(msg.sender, receiver, numTokens);
         return true;
     }
 
+
     // using a delegate account to withdraw tokens from owner's account & enable transfer
-    function approve(address delegate, uint numTokens) public returns (bool) {
+    function approve(address delegate, uint numTokens)
+        public
+        onlyOwner
+        returns (bool)
+    {
         allowed[msg.sender][delegate] = numTokens; // no. of tokens delegate is approved to help transfer
         emit Approval(msg.sender, delegate, numTokens); // delegate is approved
         return true;
     }
 
-    function allowance(address owner, address delegate) public view returns (uint) {
-        return allowed[owner][delegate]; 
+    function allowance(address delegate)
+        public
+        view
+        returns (uint)
+    {
+        return allowed[owner][delegate];
     }
 
-    function transferFrom(address owner, address investor, uint numTokens) public returns (bool) {
-        require(numTokens <= balance[owner]); // owner must have enough tokens for transfer   
-        require(numTokens <= allowed[owner][msg.sender]); // delegate must have allowance for transfer amount
-    
+    function transferFrom(address investor, uint numTokens)
+        public
+        ownerSufficientBalance(numTokens)
+        delegateSufficientBalance(numTokens)
+        returns (bool)
+    {
         balance[owner] = balance[owner]-numTokens;
         // delegate can divide allowance into multiple transactions until balance is 0
         allowed[owner][msg.sender] = allowed[owner][msg.sender]-numTokens; // subtract tokens from delegate’s allowance
         balance[investor] = balance[investor] + numTokens; // investor receives tokens
-        emit Transfer(owner, investor, numTokens); // transfer takes place 
+        emit Transfer(owner, investor, numTokens); // transfer takes place
         return true;
     }
 }
